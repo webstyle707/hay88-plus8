7983a71274103735c4459f032bbea1b9b100deb0
webdev <devweb8899@gmail.com>
2024-05-27 15:46:15 +0700

    update

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// // for slide fade
-// document.addEventListener('DOMContentLoaded', function () {
-//   new Splide('#splide1', {
-//     type: 'loop', // slide loop fade
-//     perPage: 1,
-//     autoplay: true,
-//     interval: 2000,
-//   }).mount();
-// });
-
 
 var swiper = new Swiper(".cubeSwiper3", {
   slidesPerView: 1,
@@ -87,18 +77,12 @@ function scrollToTop() {
 }
 
 
-
-
-
-
-
 function clearAllClassNames() {
   var cards = document.querySelectorAll('.scaleAnimationCard');
   cards.forEach(function(card) {
     card.classList.remove('scaleAnimationCard');
   });
 }
-
 
 function addAllClassNames() {
   var Addcard = document.querySelectorAll('.Addcard');
